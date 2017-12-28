96e80ecbd5d2b1050b5ab8e4c7f34ebff0c8b827
patrickatcw <pj8392@yahoo.com>
2017-12-28 14:24:47 -0500

    started js work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,119 @@
-   
+$("#home").on('click', function()   
+	 {
+		window.location.href= 'http:englishrussia.com/';
+			});
+
+$("#login").on('click', function()   
+	 {
+		window.location.href= 'http:englishrussia.com/wp-login.php';
+			});
+
+$("#about").on('click', function()   
+	 {
+		window.location.href= 'http:englishrussia.com/about';
+			});
+
+$("#youtube").on('click', function()   
+	 {
+		window.location.href= 'https://www.youtube.com/user/englishrussiatv';
+			});
+
+$("#facebooknav").on('click', function()   
+	 {
+		window.location.href= 'https://www.facebook.com/English-Russia-219469644747681/';
+			});
+
+$("#join").on('click', function()   
+	 {
+		window.location.href= 'http:englishrussia.com/register-3/';
+			});
+
+$("#twitter").on('click', function()   
+	 {
+		window.location.href= 'https://twitter.com/englishrussia1';
+			});
+
+$("#facebook").on('click', function()   
+	 {
+		window.location.href= 'https://www.facebook.com/English-Russia-219469644747681/';
+			});
+
+$("#email").on('click', function()   
+	 {
+		window.location.href= 'https://twitter.com/englishrussia/#';
+			});
+
+$("#rss").on('click', function()   
+	 {
+		window.location.href= 'http://feeds.feedburner.com/englishrussia/GrPQ';
+			});
+
+$("#article1").on('click', function()   
+	 {
+		window.location.href= 'http://englishrussia.com/2017/12/19/soviet-happiness-photos-of-soviet-people-happily-spending-their-free-time/';
+			});
+
+$("#article2").on('click', function()   
+	 {
+		window.location.href= 'http://englishrussia.com/2017/12/18/examples-of-soviet-cars-modifications-that-were-popular-back-then-in-ussr/';
+			});
+
+$("#article3").on('click', function()   
+	 {
+		window.location.href= 'http://englishrussia.com/2017/12/18/another-selection-of-weird-and-interesting-pictures-from-russia/';
+			});
+
+$("#article4").on('click', function()   
+	 {
+		window.location.href= 'http://englishrussia.com/2017/12/17/ice-trapped-cars-of-russia/';
+			});
+
+$("#article5").on('click', function()   
+	 {
+		window.location.href= 'http://englishrussia.com/2017/12/17/70-year-old-belorussian-retired-man-doesnt-want-to-get-old-becomes-bodybuilder/';
+			});
+
+$("#article6").on('click', function()   
+	 {
+		window.location.href= 'http://englishrussia.com/2017/12/05/faces-of-war-chechnya-1999/';
+			});
+
+$("#plane").on('click', function()   
+	 {
+		window.location.href= 'http://englishrussia.com/2015/03/24/super-luxurious-russian-presidents-new-airplane-interiors/';
+			});
+
+$("#train").on('click', function()   
+	 {
+		window.location.href= 'http://englishrussia.com/2014/05/14/the-hydro-dam-tram/';
+			});
+
+$("#engine").on('click', function()   
+	 {
+		window.location.href= 'http://englishrussia.com/2009/09/21/an-engine-replacement/';
+			});
+
+$("#milpow").on('click', function()   
+	 {
+		window.location.href= 'http://englishrussia.com/2014/01/15/the-last-breath-of-the-soviet-military-power/';
+			});
+
+$("#mig").on('click', function()   
+	 {
+		window.location.href= 'http://englishrussia.com/2008/10/20/the-mig-story/';
+			});
+
+$("#sausage").on('click', function()   
+	 {
+		window.location.href= 'http://englishrussia.com/2014/03/11/sausage-delivery-moldavian-way/';
+			});
+
+
+
+
+
+
+
+
+
+	 
